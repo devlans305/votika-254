7b70d3d6dd579ff22637c95eb3d7c46fea04a886
Evance Onyango <devlans305@gmail.com>
2024-09-05 18:07:22 +0300

    added the votika entry contract

---
+++ votikaContract.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity  ^0.8.0;
+
+Contract Votika {
+    struct Candidate{
+        string name;
+        string county;
+        string constituency;
+        string pollingStation;
+        string idNumber;
+        uint  voteCount;
+
+    }
+
+
+}
